72dcce2a759776ecfb05743a10b83d0334d01d2a
caselongo <caselongo@gmail.com>
2022-02-26 22:31:52 +0100

    v9 -> v10

---
+++ AccessibleCustomers.go
@@ -0,0 +1,28 @@
+package googleads
+
+import (
+	"net/http"
+
+	errortools "github.com/leapforce-libraries/go_errortools"
+	go_http "github.com/leapforce-libraries/go_http"
+)
+
+type AccessibleCustomers struct {
+	ResourceNames []string `json:"resourceNames"`
+}
+
+func (service *Service) ListAccessibleCustomers() (*AccessibleCustomers, *errortools.Error) {
+	accessibleCustomers := AccessibleCustomers{}
+
+	requestConfig := go_http.RequestConfig{
+		Method:        http.MethodGet,
+		Url:           service.url("customers:listAccessibleCustomers"),
+		ResponseModel: &accessibleCustomers,
+	}
+	_, _, e := service.httpRequest(&requestConfig)
+	if e != nil {
+		return nil, e
+	}
+
+	return &accessibleCustomers, nil
+}
